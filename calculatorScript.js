3683e5d7c73012fd40e58814c1e9b623fc9d2a28
Cremona Lambert <cremonalambertmw@gmail.com>
2022-12-06 15:11:36 +0200

    general review

--- calculatorScript.js
+++ calculatorScript.js
@@ -1,11 +1,53 @@
-const getvalues = document.querySelector('.buttons').addEventListener('click', function(e){
-const value = e.target.innerText;
-// const btn1 =document.querySelector('#one').value;
+(function(){
+    //get element from html file
 
-const sreenHtml = `<p>${value}</p>`;
-// console.log(sreenHtml);
+    let display = document.getElementById('display');
+    // console.log(display.value);
+    let buttons = document.querySelectorAll('.btn');
+    let del = document.querySelector('#delete');
+    let clear = document.querySelector('#clear');
+    let equal = document.querySelector('#equal');
+    let answer = document.querySelector('#ans');
+    let divBy = document.querySelector('#div_by');
 
-const display = document.querySelector('.display p').innerText = `${value}`;
+    //how to get all the values per click
 
-console.log(display);
-});
+    buttons.forEach(function(btn){
+        btn.addEventListener('click', function(e){
+            onScreen = e.target.dataset.val;
+            display.value = display.value + onScreen;
+        })
+    })
+
+    // how to setup equal button
+
+    equal.addEventListener('click', function(e){
+        if(display.value === ''){
+                display.value = "";
+        }else{
+            let result = eval(display.value);
+            display.value = result;
+        }
+    })
+
+    // how to clear value on the screen 
+
+    clear.addEventListener('click', function(e){
+        display.value = "";
+    })
+
+    //how to delete one number at a time on the screen
+
+    del.addEventListener('click', function(e){
+        display.value = display.value.slice(0,-1);
+    })
+
+    // how to display previous operation on screen
+
+    answer.addEventListener('click', function(e){
+        let ans = eval(display.value)
+        display.value = ans;
+    })
+
+  
+})();
