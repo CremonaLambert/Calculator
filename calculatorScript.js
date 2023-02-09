bc21bca70fe28f0e1a1ff3c3ba4067aff9b55779
Cremona Lambert <cremonalambertmw@gmail.com>
2023-02-09 04:17:43 +0200

    html to vanilla js

--- calculatorScript.js
+++ calculatorScript.js
@@ -49,5 +49,56 @@
         display.value = ans;
     })
 
-  
 })();
+
+/*
+ <div class="innercontainer">
+        <div class="display">
+            <input type="text" id="display" value="">
+        </div>
+       
+        <div class="keyboard">
+            <div class="buttons">
+                <button id="delete">DEL</button>
+                <button id="clear">AC</button> 
+                <button id="div_by">%</button>
+                <button class="btn" data-val="/">/</button>
+                <button class="btn" data-val="7">7</button>
+                <button class="btn" data-val="8">8</button>
+                <button class="btn" data-val="9">9</button>
+                <button class="btn" data-val="*">*</button>
+                <button class="btn" data-val="4">4</button>
+                <button class="btn" data-val="5">5</button>
+                <button class="btn" data-val="6">6</button>
+                <button class="btn" data-val="-">-</button>
+                <button class="btn" data-val="1">1</button>
+                <button class="btn" data-val="2">2</button>
+                <button class="btn" data-val="3">3</button>
+                <button class="btn" data-val="+">+</button>
+                <button class="btn" data-val="0">0</button>
+                <button class="btn" data-val=".">.</button>
+                <button id="ans">Ans</button>
+                <button id="equal">=</button>
+                            
+            </div>
+        </div>
+</div>
+*/
+
+/*<div class="innercontainer">*/
+const innercontainer = document.createElement('div');
+innercontainer.className = 'innercontainer';
+//---------------------
+/*
+    <div class="display">
+        <input type="text" id="display" value="">
+    </div>
+*/
+const display = document.createElement('div');
+display.className = 'display';
+
+const screenDisplay = document.createElement('input');
+screenDisplay.type = 'text';
+screenDisplay.id = 'display';
+screenDisplay.value = '';
+//------------------------
